434cd93ce02e7a2d2a13560b73a492dae5742d54
Wei Zhang <veeamd@gmail.com>
2021-01-08 12:30:20 -0500

    IOS-6516: add support for SPM

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "SVProgressHUD",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "SVProgressHUD", targets: ["SVProgressHUD"]),
+    ],
+    targets: [
+        .target(name: "SVProgressHUD",
+                path: "SVProgressHUD",
+                publicHeadersPath: "")
+    ]
+)
